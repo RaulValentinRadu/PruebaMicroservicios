8e7b3ea4bc8678e7fabef85ec4058480c91b7b66
AngelZhang159 <angelzhang159@gmail.com>
2024-12-09 12:26:57 +0100

    Juego

--- Juego/src/main/java/com/proyectos/spring/JuegoApplication.java
+++ Juego/src/main/java/com/proyectos/spring/JuegoApplication.java
@@ -2,8 +2,10 @@ package com.proyectos.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JuegoApplication {
 
 	public static void main(String[] args) {
