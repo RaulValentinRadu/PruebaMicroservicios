4b4e32a210dd93ccf716139ab9377cd2b474f8b7
AngelZhang159 <160955222+AngelZhang159@users.noreply.github.com>
2024-12-09 10:58:04 +0100

    Archivos base

---
+++ Biblioteca/src/main/java/com/proyectos/spring/BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectos.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
